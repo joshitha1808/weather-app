7c8d2d73ac83d928560c7cbd9b74e54b27cdfa16
joshitha0818 <joshitha0818@gmail.com>
2025-12-09 03:01:35 +0530

    refactor: simplify theme configuration in MyApp for better maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF0A0F1F),
-        cardColor: const Color(0xFF11172B),
-
-        colorScheme: const ColorScheme.dark(
-          primary: Color(0xFF7ED2FF), // sky blue
-          secondary: Color(0xFFC084FC), // purple glow
-        ),
-
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(color: Color(0xFFEAF4FF)),
-          bodyLarge: TextStyle(color: Color(0xFFEAF4FF)),
-        ),
-      ),
-
+      theme: ThemeData.dark(useMaterial3: true),
       home: const WeatherScreen(),
     );
   }
